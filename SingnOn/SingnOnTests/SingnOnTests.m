1383233cd1b64a0d2c6c14411790e71d4c8ac392
Robert DeNicola <thedizzle23@gmail.com>
2012-06-09 20:45:36 -0400

    here

---
+++ SingnOn/SingnOnTests/SingnOnTests.m
@@ -0,0 +1,32 @@
+//
+//  SingnOnTests.m
+//  SingnOnTests
+//
+//  Created by Robert DeNicola on 6/9/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "SingnOnTests.h"
+
+@implementation SingnOnTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SingnOnTests");
+}
+
+@end
